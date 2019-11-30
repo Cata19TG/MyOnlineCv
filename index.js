88b913b6d81f08bccda8b86fb0da87a295c51839
Cata19TG <tidor.catalin@yahoo.com>
2019-11-30 20:11:38 +0200

    MyOnlineCv

---
+++ index.js
@@ -0,0 +1,5 @@
+var contact = document.getElementById('#contact');
+var contact2 = document.getElementById('footer-h5');
+function scroll1() {
+    contact.addEventListener('click', window.location = contact2);
+}
