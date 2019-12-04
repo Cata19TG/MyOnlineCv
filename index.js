dd5c0607a2a493f4522170f0a6a78bb3533be408
Cata19TG <tidor.catalin@yahoo.com>
2019-12-04 12:37:48 +0200

    Add updates and redesign

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
-var contact = document.getElementById('#contact');
-var contact2 = document.getElementById('footer-h5');
-function scroll1() {
-    contact.addEventListener('click', window.location = contact2);
+// var contact = document.getElementById('header-contact');
+// var contact2 = document.getElementById('contact');
+// function scroll1() {
+//     contact.addEventListener('click', window.location.href = contact2);
+// }
+
+// scroll1();
+
+window.onscroll = function() {myFunction()};
+
+var header = document.getElementById("header");
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
 }
